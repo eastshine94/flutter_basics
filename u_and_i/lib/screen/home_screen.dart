e58ba8e8e739956be0b86df7b8d41058cd1ca8c0
hdkim <eastshine94@naver.com>
2023-05-29 18:18:19 +0900

    UI 디자인

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -13,40 +13,66 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
         backgroundColor: Colors.pink[100],
         body: SafeArea(
+          bottom: false,
           child: SizedBox(
             width: MediaQuery.of(context).size.width,
-            child: Column(children: [
-              const Text("U&I",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'parisienne',
-                      fontSize: 80.0)),
-              const Text(
-                '우리 처음 만난 날',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'sunflower',
-                    fontSize: 30.0),
-              ),
-              const Text(
-                '2021.12.27',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'sunflower',
-                    fontSize: 20.0),
-              ),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.favorite)),
-              const Text(
-                'D+1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.w700,
-                ),
-              )
+            child: Column(children: const [
+              _TopPart(),
+              _BottomPart(),
             ]),
           ),
         ));
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        const Text("U&I",
+            style: TextStyle(
+                color: Colors.white, fontFamily: 'parisienne', fontSize: 80.0)),
+        Column(
+          children: const [
+            Text(
+              '우리 처음 만난 날',
+              style: TextStyle(
+                  color: Colors.white, fontFamily: 'sunflower', fontSize: 30.0),
+            ),
+            Text(
+              '2021.12.27',
+              style: TextStyle(
+                  color: Colors.white, fontFamily: 'sunflower', fontSize: 20.0),
+            )
+          ],
+        ),
+        IconButton(
+            onPressed: () {},
+            iconSize: 60.0,
+            icon: const Icon(Icons.favorite, color: Colors.red)),
+        const Text(
+          'D+1',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'sunflower',
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+        )
+      ]),
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Image.asset('asset/img/middle_image.png'));
   }
 }
