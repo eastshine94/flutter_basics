602942e91a704b7aa1bdbf3d39a8e60de36b2ffc
hdkim <eastshine94@naver.com>
2023-05-29 15:24:47 +0900

    stateful widget

---
+++ stateful_widget/lib/screen/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  final Color color;
+
+  const HomeScreen({
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: widget.color,
+    );
+  }
+}
+
+// class HomeScreen extends StatelessWidget {
+//   final Color color;
+
+//   const HomeScreen({
+//     required this.color,
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 50.0,
+//       height: 50.0,
+//       color: color,
+//     );
+//   }
+// }
