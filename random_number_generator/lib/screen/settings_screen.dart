35443ca8faf78cd0f6ddf8cacc3494f92947a90a
hdkim <eastshine94@naver.com>
2023-05-31 23:08:54 +0900

    slider 위젯 사용

--- random_number_generator/lib/screen/settings_screen.dart
+++ random_number_generator/lib/screen/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -8,11 +9,45 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 10000;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Settings screen"),
+    return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            Expanded(
+              child: Row(
+                children: maxNumber
+                    .toInt()
+                    .toString()
+                    .split('')
+                    .map((x) => Image.asset(
+                          'asset/img/$x.png',
+                          width: 50.0,
+                          height: 70.0,
+                        ))
+                    .toList(),
+              ),
+            ),
+            Slider(
+                value: maxNumber,
+                min: 10000,
+                max: 1000000,
+                onChanged: (double val) {
+                  setState(() {
+                    maxNumber = val;
+                  });
+                }),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
+                child: const Text('저장!'))
+          ]),
+        ),
       ),
     );
   }
