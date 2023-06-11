a5e8d2db5a07486aab7b4f0bb0fa8d964acc667b
hdkim <eastshine94@naver.com>
2023-06-11 23:52:46 +0900

    navigation 기본

---
+++ navigation/lib/screen/route_three_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RouteThreeScreen extends StatelessWidget {
+  const RouteThreeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Route Three')),
+    );
+  }
+}
---
+++ navigation/lib/screen/route_two_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RouteTwoScreen extends StatelessWidget {
+  const RouteTwoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Route Two')),
+    );
+  }
+}
