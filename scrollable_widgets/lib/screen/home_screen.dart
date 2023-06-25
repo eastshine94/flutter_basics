6241796e5c0599d390c9215187967f02e6e2bf82
hdkim <eastshine94@naver.com>
2023-06-25 23:47:52 +0900

    GridView

--- scrollable_widgets/lib/screen/home_screen.dart
+++ scrollable_widgets/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scrollable_widgets/layout/main_layout.dart';
+import 'package:scrollable_widgets/screen/grid_view_screen.dart';
 import 'package:scrollable_widgets/screen/list_view_screen.dart';
 import 'package:scrollable_widgets/screen/single_child_scroll_view_screen.dart';
 
@@ -16,6 +17,7 @@ class HomeScreen extends StatelessWidget {
         builder: (_) => SingleChildScrollViewScreen(),
         name: "SingleChildScrollViewScreen"),
     ScreenModel(builder: (_) => ListViewScreen(), name: "ListViewScreen"),
+    ScreenModel(builder: (_) => GridViewScreen(), name: "GridViewScreen"),
   ];
 
   HomeScreen({Key? key}) : super(key: key);
