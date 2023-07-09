c1183efbaeb4ea8e59c55a98d4167ebb6559365d
hdkim <eastshine94@naver.com>
2023-07-09 23:29:45 +0900

    tabbar 스타일링

--- tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
+++ tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
@@ -7,16 +7,42 @@ class BasicAppbarTabbarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: TABS.length,
+        length: TABS.length * 3,
         child: Scaffold(
           appBar: AppBar(
             title: const Text("BasicAppBarScreen"),
-            bottom: TabBar(
-                tabs: TABS
-                    .map((e) => Tab(icon: Icon(e.icon), child: Text(e.label)))
-                    .toList()),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(100),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TabBar(
+                      indicatorColor: Colors.red,
+                      indicatorWeight: 4.0,
+                      // tab: 전체 사이즈
+                      // label: label 영역만
+                      indicatorSize: TabBarIndicatorSize.label,
+                      isScrollable: true,
+                      labelColor: Colors.yellow,
+                      unselectedLabelColor: Colors.black,
+                      labelStyle: const TextStyle(fontWeight: FontWeight.w700),
+                      unselectedLabelStyle:
+                          const TextStyle(fontWeight: FontWeight.w100),
+                      tabs: [...TABS, ...TABS, ...TABS]
+                          .map((e) =>
+                              Tab(icon: Icon(e.icon), child: Text(e.label)))
+                          .toList()),
+                ],
+              ),
+            ),
           ),
-          body: const Column(children: []),
+          body: TabBarView(
+              physics: const NeverScrollableScrollPhysics(),
+              children: [...TABS, ...TABS, ...TABS]
+                  .map((e) => Center(
+                        child: Icon(e.icon),
+                      ))
+                  .toList()),
         ));
   }
 }
